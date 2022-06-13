e8b84b60694b374cdfd243117b566ab2b082fdd3
Mykolas Raudžius <raudziusmykolas@gmail.com>
2022-06-13 18:00:51 +0300

    feat: shopping cart

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,6 +7,20 @@ class Product {
   }
 }
 
+class ShoppingCart {
+  items = [];
+
+  render() {
+    const cartEl = document.createElement('section');
+    cartEl.innerHTML = `
+    <h2>Total: \$${0}
+    <button>Order now!</button>
+    `;
+    cartEl.className = 'cart';
+    return cartEl;
+  }
+}
+
 class ProductItem {
   constructor(product) {
     this.product = product;
@@ -39,17 +53,16 @@ class ProductItem {
 
 class ProductList {
   products = [
-    new Product('A Pillow', 'https://contents.mediadecathlon.com/p1749048/f0b275c3207e208e12771a5c385d3ff8/p1749048.jpg?format=auto&quality=70&f=1024x0', 19.99, 'A soft pillow!'),
+    new Product('A Pillow', 'https://contents.mediadecathlon.com/p1749048/f0b275c3207e208e12771a5c385d3ff8/p1749048.jpg?format=auto&quality=70&f=1024x0', 'A soft pillow!', 19.99),
     new Product(
       'A Carpet',
       'https://marvel-b1-cdn.bc0a.com/f00000000083977/www.coit.com/sites/default/files/styles/original/public/media/2018-10/cofee-stain-card-large.jpg?itok=I1QoE5H4',
-      89.99,
-      'A which you might like - or not'
+      'A carpet which you might like - or not',
+      89.99
     ),
   ];
 
   render() {
-    const renderHook = document.getElementById('app');
     const prodList = document.createElement('ul');
     prodList.className = 'product-list';
     for (const prod of this.products) {
@@ -57,9 +70,22 @@ class ProductList {
       const prodEl = productItem.render();
       prodList.append(prodEl);
     }
-    renderHook.append(prodList);
+    return prodList;
   }
 }
 
-const productList = new ProductList();
-productList.render();
+class Shop {
+  render() {
+    const renderHook = document.getElementById('app');
+
+    const cart = new ShoppingCart();
+    const cartEl = cart.render();
+    const productList = new ProductList();
+    const prodListEl = productList.render();
+
+    renderHook.append(cartEl, prodListEl);
+  }
+}
+
+const shop = new Shop();
+shop.render();
